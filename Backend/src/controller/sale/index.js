fea26a922ef361c82775f58f695cae15a19b8223
AbdulRehmanZia <abdulrehmanzia2021@gmail.com>
2025-08-05 18:16:35 +0500

    Fix sale router bug

--- Backend/src/controller/sale/index.js
+++ Backend/src/controller/sale/index.js
@@ -3,7 +3,8 @@
 import { getAllSales } from "./get/index.js";
 import { addSale } from "./post/index.js";
 import { deleteSale } from "./delete/index.js";
+import { saleSearch } from "./get/index.js";
 
 //Exporting All Controllers
 
-export { getAllSales, addSale, deleteSale };
+export { getAllSales, addSale, deleteSale,saleSearch };
--- Backend/src/routes/sale.route.js
+++ Backend/src/routes/sale.route.js
@@ -2,11 +2,12 @@ import { Router } from "express";
 
 import { verifyJWT } from "../middleware/authMiddleware.js";
 import authorizeRole from "../middleware/authorizeRoles.js";
-import { addSale, deleteSale, getAllSales } from "../controller/sale/index.js";
+import { addSale, deleteSale, getAllSales, saleSearch } from "../controller/sale/index.js";
 
 const router = Router();
 
-router.get("/", getAllSales);
+router.get("/", verifyJWT, getAllSales);
+router.get("/search", verifyJWT, saleSearch);
 router.post("/add-sale", verifyJWT, addSale);
 
 router.delete(
